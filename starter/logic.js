6f2cfb703984377e02431a5958ffd0728ae6f5e9
grace-apa <149172807+grace-apa@users.noreply.github.com>
2024-01-01 14:31:45 +0000

    first question added to logic.js file

---
+++ starter/logic.js
@@ -0,0 +1,11 @@
+const questions = [
+  {
+    question: "Commonly used data types DO not include:",
+    answers: [
+      { text: "1. Strings", correct: false },
+      { text: "2. Booleans", correct: false },
+      { text: "3. Alerts", correct: true },
+      { text: "4. Numbers", correct: false },
+    ],
+  },
+];
