d5a764ff4842c752424799ea1b08b1106793d3e5
grace-apa <149172807+grace-apa@users.noreply.github.com>
2024-01-01 14:44:58 +0000

    questions 2 and 3 added to questions.js

---
+++ starter/questions.js
@@ -0,0 +1,29 @@
+const questions = [
+  {
+    question: "Commonly used data types DO not include:",
+    answers: [
+      { text: "1. Strings", correct: false },
+      { text: "2. Booleans", correct: false },
+      { text: "3. Alerts", correct: true },
+      { text: "4. Numbers", correct: false },
+    ],
+  },
+  {
+    question: "The condition in an if/else statement is enclosed with ______.",
+    answers: [
+      { text: "1. Quotes", correct: false },
+      { text: "2. Curly brackets", correct: false },
+      { text: "3. Parenthesis", correct: true },
+      { text: "4. Square brackets", correct: false },
+    ],
+  },
+  {
+    question: "Arrays in JavaScript can be used to store_____.",
+    answers: [
+      { text: "1. Numbers and strings", correct: false },
+      { text: "2. Other arrays", correct: false },
+      { text: "3. Booleans", correct: false },
+      { text: "4. All of the above", correct: true },
+    ],
+  },
+];
