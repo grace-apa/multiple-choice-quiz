29d2ce82b751cd0c35848557cf7660bd9b8ed063
grace-apa <149172807+grace-apa@users.noreply.github.com>
2024-01-02 14:48:37 +0000

    start button hidden using classList

--- starter/assets/css/logic.js
+++ starter/assets/css/logic.js
@@ -2,7 +2,10 @@ const startButton = document.getElementById("start");
 
 startButton.addEventListener("click", startGame);
 
-function startGame() {}
+function startGame() {
+  console.log("Started");
+  startButton.classList.add("hide");
+}
 
 function setNextQuestion() {}
 
